2f4debdfcc56b4091bdd0180a2952d59ab2806c2
Otto W <wichmannoa@gmail.com>
2014-10-11 12:33:55 -0700

    First binary convertor file

---
+++ Convertor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/*Created for github*/
+namespace SimpleConvertor
+{
+    class Convertor
+    {
+        static void Main(string[] args)
+        {
+            char[] characters;
+            Console.WriteLine("What is your name, friend? ");
+            characters = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Char\tBinary\t\tOctal\tDecimal\tHex");
+            foreach (var item in characters)
+            {               
+                Console.WriteLine(item  + "\t0" + Convert.ToString(item, 2) + "\t" + Convert.ToString(item, 8) + "\t" + Convert.ToString(item, 10) + "\t" + Convert.ToString(item, 16));
+            }
+            Console.ReadLine();
+        }
+    }
+}
